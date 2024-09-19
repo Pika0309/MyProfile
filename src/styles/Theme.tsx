2c5204b01a597dc15b8ff95d25592acbcdbe9563
Pika0309 <zhenya.shumakov.95@gmail.com>
2024-09-19 21:17:06 +0300

    first commit

---
+++ src/styles/Theme.tsx
@@ -0,0 +1,21 @@
+
+
+type ThemePropsType = {
+    colors:{
+        bcgGroundSection: '#343D68',
+        colorTextButton: '#FFFFFFDE',
+        colorTextTitle: '#E84949',
+        colorTextName: '#343D68',
+        coloTextDescriptTech: '#818080',
+        colorTextTechStack: '#000000'
+    }
+}
+
+import React from 'react';
+
+export const Theme = () => {
+    return (
+        <div>
+            </div>
+    );
+};
